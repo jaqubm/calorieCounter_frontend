ee58ca21e1b1632f576283fd2b4e0e56ce6a308a
b4rtm <bartek.matusiak12@gmail.com>
2024-10-21 11:21:54 +0200

    implement home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
-import 'package:caloriecounter/screens/LoginScreen.dart';
+import 'package:caloriecounter/screens/home_screen.dart';
+import 'package:caloriecounter/screens/login_screen.dart';
+import 'package:caloriecounter/screens/products_screen.dart';
+import 'package:caloriecounter/screens/profile_screen.dart';
+import 'package:caloriecounter/screens/recipes_screen.dart';
+import 'package:caloriecounter/widgets/bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -13,9 +18,41 @@ class CalorieCounterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-        home: LoginScreen(),
+        home: HomePage(),
     );
   }
-  
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    RecipesScreen(),
+    ProductsScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: _currentIndex,
+        onItemTapped: _onItemTapped,
+      ),
+    );
+  }
 }
 
