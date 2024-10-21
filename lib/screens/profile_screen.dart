162f6384859fe55d1ac5cbf8b3177512c261008d
b4rtm <bartek.matusiak12@gmail.com>
2024-10-21 12:18:34 +0200

    add log out button to profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,11 +1,43 @@
+import 'package:caloriecounter/screens/login_screen.dart';
+import 'package:caloriecounter/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
 
 class ProfileScreen extends StatelessWidget {
+  final AuthService _authService = AuthService(); // Inicjalizacja AuthService
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Screen', style: TextStyle(fontSize: 24)),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('User Profile Information'),
+            SizedBox(height: 20),
+            
+            GestureDetector(
+              onTap: () async {
+                await _authService.handleSignOut();
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                  (Route<dynamic> route) => false,
+                );
+              },
+              child: Column(
+                children: [
+                  Icon(Icons.logout, size: 40, color: Colors.red),
+                  Text('Log Out', style: TextStyle(color: Colors.red)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
