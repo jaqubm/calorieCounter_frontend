bcf3cc2c39b8c924c31204f1548f22d1ff0f110c
Maksymilian <maksim2@op.pl>
2024-10-21 21:22:17 +0200

    dish widget view

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/dish_section.dart';
+
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Home Screen', style: TextStyle(fontSize: 24)),
+      child: MealCard(
+          mealType: 'Breakfast',
+          totalCalories: 945,
+          ingredients: [
+            {'name': 'Cereal with milk', 'calories': 545},
+            {'name': 'Sausage', 'calories': 300},
+            {'name': 'Banana', 'calories': 100},
+          ],
+        ),
     );
   }
 }
