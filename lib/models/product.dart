4b7b0453ca219ae66ec52008b6657e9009d6308e
b4rtm <bartek.matusiak12@gmail.com>
2024-10-24 13:11:56 +0200

    create product model

---
+++ lib/models/product.dart
@@ -0,0 +1,11 @@
+class Product {
+  final String name;
+  final String valuesPer;
+  final String energy;
+
+  Product({
+    required this.name,
+    required this.valuesPer,
+    required this.energy,
+  });
+}
