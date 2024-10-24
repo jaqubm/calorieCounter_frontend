77f869a3682936435bd3f74199ccbee1384b546e
b4rtm <bartek.matusiak12@gmail.com>
2024-10-24 13:04:03 +0200

    initial products screen

--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -1,11 +1,68 @@
+import 'package:caloriecounter/widgets/search_input.dart';
 import 'package:flutter/material.dart';
 
 
-class ProductsScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import '../widgets/search_input.dart';
+
+class ProductsScreen extends StatefulWidget {
+  @override
+  _ProductsScreenState createState() => _ProductsScreenState();
+}
+
+class _ProductsScreenState extends State<ProductsScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  List<String> _allProducts = ['Apple', 'Banana', 'Orange', 'Mango', 'Grapes'];
+  List<String> _filteredProducts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredProducts = _allProducts;
+  }
+
+  void _filterProducts(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        _filteredProducts = _allProducts;
+      } else {
+        _filteredProducts = _allProducts
+            .where((product) =>
+                product.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Products Screen', style: TextStyle(fontSize: 24)),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Products'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SearchInput(
+              hintText: 'Search products...',
+              controller: _searchController,
+              onChanged: _filterProducts,
+              onSubmitted: _filterProducts,
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredProducts.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_filteredProducts[index]),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
