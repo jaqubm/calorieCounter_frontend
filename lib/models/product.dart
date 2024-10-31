8213370964e3d9636c4ed707e99de0e0964e50ff
b4rtm <bartek.matusiak12@gmail.com>
2024-10-31 16:45:13 +0100

    update product model

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,11 +1,19 @@
 class Product {
   final String name;
-  final String valuesPer;
-  final String energy;
+  final int valuesPer;
+  final int energy;
+  final int protein;
+  final int carbohydrates;
+  final int fat;
+  final String ownerEmail;
 
   Product({
     required this.name,
     required this.valuesPer,
     required this.energy,
+    required this.protein,
+    required this.carbohydrates,
+    required this.fat,
+    required this.ownerEmail,
   });
 }
