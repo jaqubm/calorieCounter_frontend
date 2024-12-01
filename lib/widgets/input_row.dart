cd3b96f4da1c0488e7f10f99a975a081c3b8352d
b4rtm <bartek.matusiak12@gmail.com>
2024-12-01 14:07:56 +0100

    fix readonly product view

--- lib/widgets/input_row.dart
+++ lib/widgets/input_row.dart
@@ -12,6 +12,7 @@ Widget InputRow(
     child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
+        // Label
         Container(
           width: 100,
           child: Text(
@@ -22,44 +23,62 @@ Widget InputRow(
             ),
           ),
         ),
+        // Input field
         Expanded(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 9.0),
-            child: TextFormField(
-              controller: controller,
-              readOnly: isReadOnly,
-              decoration: InputDecoration(
-                border: isReadOnly
-                    ? InputBorder.none
-                    : OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                        borderSide: BorderSide(
+            child: isReadOnly
+                ? Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        controller.text,
+                        style: TextStyle(
+                          fontSize: 14,
                           color: Colors.black,
-                          width: 2.5,
                         ),
                       ),
-                focusedBorder: isReadOnly
-                    ? InputBorder.none
-                    : OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: BorderSide(
-                          color: Colors.blue,
-                          width: 2.0,
-                        ),
-                      ),
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
-              ),
-              keyboardType:
-                  label == 'Name' ? TextInputType.text : TextInputType.number,
-              validator: validator,
-            ),
+                      SizedBox(width: 8),
+                      Text(unit),
+                    ],
+                  )
+                : TextFormField(
+                    controller: controller,
+                    readOnly: isReadOnly,
+                    decoration: InputDecoration(
+                      border: isReadOnly
+                          ? InputBorder.none
+                          : OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                                width: 2.5,
+                              ),
+                            ),
+                      focusedBorder: isReadOnly
+                          ? InputBorder.none
+                          : OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: BorderSide(
+                                color: Colors.blue,
+                                width: 2.0,
+                              ),
+                            ),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+                    ),
+                    keyboardType: label == 'Name'
+                        ? TextInputType.text
+                        : TextInputType.number,
+                    validator: validator,
+                  ),
           ),
         ),
-        SizedBox(
-          width: 50,
-          child: Text(unit),
-        ),
+        if (!isReadOnly)
+          SizedBox(
+            width: 50,
+            child: Text(unit),
+          ),
       ],
     ),
   );
